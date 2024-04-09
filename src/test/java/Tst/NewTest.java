0d58e25494acb18ea350b9d1672c3c66428c7d2d
Shivam <shri.shivam27@gmail.com>
2024-04-09 11:32:05 +0530

    test

---
+++ src/test/java/Tst/NewTest.java
@@ -0,0 +1,50 @@
+package Tst;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	WebDriver driver  = null;
+	
+  @BeforeSuite
+  public void f() {
+	  
+	  System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+	  
+	  driver  = new ChromeDriver();
+	  
+	  driver.get("https://www.bmw.in/en/");
+	  
+  }
+  
+  
+  @Test(priority = 1)
+  public void test() {
+	  
+	  driver.get("https://www.bmw.in/en/all-models.html");
+	  Assert.fail();
+	  
+  }
+  
+  
+
+  @Test(priority = 2)
+  public void test2() {
+	  driver.get("https://www.bmw.in/en/topics/owners/joy-rewards.html");
+	  System.out.println("pass");
+	  
+  }
+  
+  @AfterSuite
+  public void after() {
+	  
+	  driver.close();
+	  
+  }  
+  
+}
